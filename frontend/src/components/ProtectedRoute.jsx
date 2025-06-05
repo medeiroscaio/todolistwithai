80006be05a57434b623c9f04c314257dfe257352
medeiroscaio <caiovictor.medeiros50@gmail.com>
2025-06-05 13:44:19 -0300

    att 40

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ children }) => {
 
   useEffect(() => {
     axios
-      .get(`http://${localURL}:5000/api/tasks`, { withCredentials: true })
+      .get(`${localURL}:5000/api/tasks`, { withCredentials: true })
       .then(() => {
         setIsAuthenticated(true);
       })
