0d350e5cc5a395b19c06a4eec997154120fdf5ef
medeiroscaio <caiovictor.medeiros50@gmail.com>
2025-06-04 23:49:44 -0300

    att 7,0

--- frontend/src/assets/httpService/httpService.js
+++ frontend/src/assets/httpService/httpService.js
@@ -1,4 +1,5 @@
 export const localURL = "18.224.67.75"; // ou "52.91.123.45", ou "api.meudominio.com"
+import axios from "axios";
 
 const api = axios.create({
   baseURL: `http://${localURL}:5000/`,
