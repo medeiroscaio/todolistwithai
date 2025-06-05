cd6dd8711575c9894e86f21f181ff14009ff3c73
medeiroscaio <caiovictor.medeiros50@gmail.com>
2025-06-05 00:01:31 -0300

    att 8.0

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,20 @@ import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
+import { localURL } from "../frontend/src/assets/httpService/httpService.js";
 
 dotenv.config();
 
 const app = express();
-
+const localURL = "http://18.224.67.75";
 app.use(cookieParser());
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: localURL,
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
